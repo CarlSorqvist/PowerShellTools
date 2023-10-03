0f231968d89a186d2d9bcb26a426fada124be2bd
Carl Sörqvist <112415522+CarlSorqvist@users.noreply.github.com>
2023-10-03 12:56:58 +0200

    Add files via upload

---
+++ LinqSort.ps1
@@ -0,0 +1,24 @@
+﻿$Code = @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class LinqPowerShell
+{
+    public static IEnumerable<T> Sort<T>(this IEnumerable<T> source)
+    {
+        return source.Sort(false);
+    }
+    public static IEnumerable<T> Sort<T>(this IEnumerable<T> source, bool descending)
+    {
+        return source.Sort(descending, Comparer<T>.Default);
+    }
+    public static IEnumerable<T> Sort<T>(this IEnumerable<T> source, bool descending, IComparer<T> comparer)
+    {
+        return descending ? source.OrderByDescending(k => k, comparer) : source.OrderBy(k => k, comparer);
+    }
+}
+"@
+Add-Type -TypeDefinition $Code -Language CSharp -ReferencedAssemblies mscorlib, System.Security -ErrorAction Stop
+
+# [LinqPowerShell]::Sort($Array)
